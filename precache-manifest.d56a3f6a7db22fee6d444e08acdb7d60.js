e0ad75a3db6a55612755ec49105688b38a8e6b49
Shivam Agrawal <shivam301296@gmail.com>
2020-02-16 10:27:07 +0530

    Updates

---
+++ precache-manifest.d56a3f6a7db22fee6d444e08acdb7d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81898cbc3422144f21ac9c1fb2ad126c",
+    "url": "/morsify/index.html"
+  },
+  {
+    "revision": "b3b78567082075b5c030",
+    "url": "/morsify/static/css/main.bbb0cf2b.chunk.css"
+  },
+  {
+    "revision": "b06fa4a2696fd6fab738",
+    "url": "/morsify/static/js/2.fc30da56.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/morsify/static/js/2.fc30da56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b78567082075b5c030",
+    "url": "/morsify/static/js/main.dcb37255.chunk.js"
+  },
+  {
+    "revision": "4563b1020723d6f24427",
+    "url": "/morsify/static/js/runtime-main.6cb6487f.js"
+  }
+]);
