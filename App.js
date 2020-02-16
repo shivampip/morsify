8a2e29a71404e378b3c4eb4eda23d2212234d801
Shivam Agrawal <shivam301296@gmail.com>
2020-02-16 10:26:22 +0530

    initial commit

--- App.js
+++ App.js
@@ -2,7 +2,19 @@ import React from "react";
 
 class App extends React.Component {
 	render() {
-		return <div>App</div>;
+		return (
+			<div className="app">
+				<div className="title">Morse Code Translator</div>
+				<textarea
+					className="asciiTa"
+					value={this.state.text}
+					onChange={eve => {
+						this.setState({ text: eve.target.value });
+					}}
+				></textarea>
+				<textarea className="morseTa"></textarea>"
+			</div>
+		);
 	}
 }
 
--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import GithubCatRibbon from "github-cat-ribbon";
+import morseCodes from "./MorseCodes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	state = { text: "", morse: "" };
+
+	textToMorse = eve => {
+		let text = eve.target.value.toLowerCase();
+		let morse = "";
+		for (let i = 0; i < text.length; i++) {
+			let chr = text.charAt(i);
+			if (chr === " ") {
+				morse += "   ";
+			} else {
+				morse += morseCodes[chr] + " ";
+			}
+		}
+		let nmorse = morse.replace(" ", "\xa0");
+		this.setState({ text: text, morse: nmorse });
+	};
+
+	getKeyByValue = (object, value) => {
+		return Object.keys(object).find(key => object[key] === value);
+	};
+
+	morseToText = eve => {
+		let nmorse = eve.target.value.replace(" ", "\xa0");
+		let rmorse = "";
+		for (let z = 0; z < nmorse.length; z++) {
+			let ch = nmorse.charAt(z);
+			if (ch === "." || ch === "-" || ch === "\xa0") {
+				rmorse += nmorse.charAt(z);
+			}
+		}
+		let morse = rmorse.split("\xa0\xa0\xa0");
+		let text = "";
+		for (let i = 0; i < morse.length; i++) {
+			let word = morse[i].split("\xa0");
+			for (let k = 0; k < word.length; k++) {
+				let ww = word[k];
+				console.log("ww:" + ww + "," + ww.length);
+				if (ww.length > 0) {
+					text += this.getKeyByValue(morseCodes, word[k]);
+				}
+			}
+			text += " ";
+		}
+		this.setState({ text: text, morse: rmorse });
+	};
+
+	render() {
+		return (
+			<div className="app">
+				<div className="title">Live Morse Code Translator</div>
+				<textarea
+					placeholder="Enter Text Here"
+					className="textTa"
+					value={this.state.text}
+					onChange={this.textToMorse}
+				></textarea>
+				<textarea
+					placeholder="Enter Morse Code Here"
+					className="morseTa"
+					value={this.state.morse}
+					onChange={this.morseToText}
+				></textarea>
+				<GithubCatRibbon username="shivampip" reponame="morsify" />
+			</div>
+		);
+	}
 }
 
 export default App;
